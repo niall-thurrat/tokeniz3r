3dc4cfc21cd12e61b2344f1f155e4ae7ab38dc26
Niall Thurrat <nt222fc@student.lnu.se>
2021-09-15 22:20:27 +0200

    Tokenizer returns first token

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 export class Tokenizer {
-    returnString () {
-        console.log('Tokenizer test string')
+    constructor(inputStr, grammar) {
+        this.inputStr = inputStr
+        this.grammar = grammar
+      }
+
+    getFirstToken() {
+        const wordRegex = this.grammar[0].regex
+        const match = this.inputStr.match(wordRegex)
+        // match example [ 'return', index: 0, input: 'return me please.', groups: undefined ]
+
+        const token = {
+            type: 'word',
+            value: match[0]
+        }
+
+        return token
     }
 }
